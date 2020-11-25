ebfe849a09cea79af8bb026dd2ee5c4bf98d97e7
Chase Lancaster <chaseblancaster@gmail.com>
2020-11-25 15:40:10 -0800

    add NavBar.css and import it to NavBar.js

--- src/Component/NavBar/NavBar.js
+++ src/Component/NavBar/NavBar.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './NavBar.css';
+
 const NavBar = () => {
     return (
         <div className="nav-container">
-            NavBar
+            <div className="logo">
+                NoSmallStreamer
+            </div>
+            <ul>
+                <li>Search</li>
+                <li>Profile</li>
+                <li>Signin</li>
+            </ul>
         </div>
     )
 }
 
 export default NavBar;
+
