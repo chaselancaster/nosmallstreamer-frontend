574f011a923e2d086c322b5a381d55f45b27d61f
Chase Lancaster <chaseblancaster@gmail.com>
2020-11-25 15:19:32 -0800

    add NavBar to App.js to see if anything appears

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import React, { Component } from "react";
 // import { Switch, Route, withRouter } from "react-router-dom";
 
+import NavBar from './Component/NavBar/NavBar';
+
 function App() {
   return (
     <div className="App">
-      NoSmallStreamer
+      <NavBar />
     </div>
   );
 }
