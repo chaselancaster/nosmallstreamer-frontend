134651e24f090922fd74e993387788ac4d96f54b
Chase Lancaster <chaseblancaster@gmail.com>
2020-11-26 15:21:17 -0800

    Add initial p tags and div structure to the Hero.js file

---
+++ src/Component/Hero/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div className="hero-parent">
+            <div>
+                <p>Welcome to <span>NoSmallStreamer.</span></p>
+                <p>A place to discover hard working, growing streamers.</p>
+            </div>
+            <div className="logo">
+
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
