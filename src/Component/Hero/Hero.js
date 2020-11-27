97bea392fea0ef0259b858b7cdce62e262dd1b89
Chase Lancaster <chaseblancaster@gmail.com>
2020-11-26 17:14:34 -0800

    Restructure Hero.js to remove a div and use css to align items in the center including the logo

--- src/Component/Hero/Hero.js
+++ src/Component/Hero/Hero.js
@@ -6,10 +6,8 @@ import './Hero.css';
 const Hero = () => {
     return (
         <div className="hero-parent">
-            <div className="text-container">
-                <p className="welcome">Welcome to <span>NoSmallStreamer.</span></p>
-                <p>A place to discover hard working, growing streamers.</p>
-            </div>
+            <p className="welcome">Welcome to <span>NoSmallStreamer.</span></p>
+            <p>A place to discover hard working, growing streamers.</p>
             <div className="logo">
               <img src={image} />
             </div>
