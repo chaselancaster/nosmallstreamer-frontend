57dc6c8b1d9cab6b4b70946d6e2908893ee1b2c0
Chase Lancaster <chaseblancaster@gmail.com>
2020-11-26 16:43:24 -0800

    require and add image to Hero.js

--- src/Component/Hero/Hero.js
+++ src/Component/Hero/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import image from './streamer.png';
 
 import './Hero.css';
 
@@ -9,9 +10,9 @@ const Hero = () => {
                 <p className="welcome">Welcome to <span>NoSmallStreamer.</span></p>
                 <p>A place to discover hard working, growing streamers.</p>
             </div>
-            {/* <div className="logo">
-
-            </div> */}
+            <div className="logo">
+              <img src={image} />
+            </div>
         </div>
     )
 }
