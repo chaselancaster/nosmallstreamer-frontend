ac278e0d25f9846c6d8ec55ee0e0edadd2082e05
Chase Lancaster <chaseblancaster@gmail.com>
2020-11-30 13:24:23 -0800

    Change landing arrangement so imgs and paragraphs are in their own separate categories

--- src/Component/Landing/Landing.js
+++ src/Component/Landing/Landing.js
@@ -4,6 +4,7 @@ import streamerImg from './assets/streamer.png';
 import handshakeImg from './assets/hand-shake.png';
 import magnifyingImg from './assets/magnifying.png';
 import twitchImg from './assets/twitch.png';
+import magnifierImg from './assets/magnifier.png';
 
 import './Landing.css';
 
@@ -14,18 +15,34 @@ const Landing = () => {
         <div className="hero-parent">
             <p className="welcome">Welcome to <span>NoSmallStreamer.</span></p>
             <div className="hero-logo">
-              <img className="hero-img" src={streamerImg} />
+              <img className="hero-img" src={streamerImg} alt="streamer"/>
             </div>
             <p className="hero-p">A place to discover hard working, growing streamers.</p>
         </div>
         // Info Section
         <div className="info-parent">
-            <div className="info">
-                <img></img>
-                <p></p>
+            <div className="info-img-container">
+                <img className="info-img" src={magnifyingImg} alt="magnifying glass"/>
+                <img className="info-img" src={handshakeImg} alt="handshake"/>
+                <img className="info-img" src={twitchImg} alt="twitch logo"/>
             </div>
-            <div className="info"></div>
-            <div className="info"></div>
+            <div className="info">
+                <p>Find streamers hidden in popular categories!</p>
+                <p>Help new streamers grow!</p>
+                <p>Direct links to a user's channel!</p>
+            </div>
+            {/* <div className="info">
+                <img className="info-img" src={magnifyingImg} alt="magnifying glass"/>
+                <p>Find streamers hidden in popular categories!</p>
+            </div>
+            <div className="info">
+                <img className="info-img" src={handshakeImg} alt="handshake"/>
+                <p>Help new streamers grow!</p>
+            </div>
+            <div className="info">
+                <img className="info-img" src={twitchImg} alt="twitch logo"/>
+                <p>Direct links to a user's channel!</p>
+            </div> */}
         </div>
     </div>
       
