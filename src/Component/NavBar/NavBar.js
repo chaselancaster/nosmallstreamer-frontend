90400441eb4ff0e9e4bb44052ef216f6f7d9ec51
Chase Lancaster <chaseblancaster@gmail.com>
2020-11-30 15:05:40 -0800

    install react-burger-menu and add initial styling. will come back to this

--- src/Component/NavBar/NavBar.js
+++ src/Component/NavBar/NavBar.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { slide as Menu } from 'react-burger-menu'
 
 import './NavBar.css';
 
 const NavBar = () => {
     return (
-        <div className="nav-container">
-            <div className="title">
-                NoSmallStreamer
-            </div>
-            <ul>
-                <li>Search</li>
-                <li>Profile</li>
-                <li>Signin</li>
-            </ul>
-        </div>
+        <Menu right>
+            <a id="home" className="menu-item" href="/">Search</a>
+            <a id="about" className="menu-item" href="/about">Profile</a>
+            <a id="contact" className="menu-item" href="/contact">Signin</a>
+        </Menu>
+        // <div className="nav-container">
+        //     <div className="title">
+        //         NoSmallStreamer
+        //     </div>
+        //     <ul>
+        //         <li>Search</li>
+        //         <li>Profile</li>
+        //         <li>Signin</li>
+        //     </ul>
+        // </div>
     )
 }
 
