1198c04bf696ec05bad7d8457ff6fa1229482acf
Chase Lancaster <chaseblancaster@gmail.com>
2020-11-30 11:15:03 -0800

    restructure Landing with another parent div to allow a separate info-parent for the remaining info information that will be added

--- src/Component/Landing/Landing.js
+++ src/Component/Landing/Landing.js
@@ -5,14 +5,23 @@ import './Landing.css';
 
 const Landing = () => {
     return (
+    <div>
+        // Hero Section
         <div className="hero-parent">
             <p className="welcome">Welcome to <span>NoSmallStreamer.</span></p>
             <div className="hero-logo">
               <img className="hero-img" src={image} />
             </div>
             <p className="hero-p">A place to discover hard working, growing streamers.</p>
-            
         </div>
+        // Info Section
+        <div className="info-parent">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+      
     )
 }
 
