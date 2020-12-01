b31e31db342c2e20abc89718746b6d59ecbb9bb1
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-01 11:46:03 -0800

    create Login folder and .js file

---
+++ src/Component/Login/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-route-dom';
+
+class Login extends Component {
+    render() {
+        return (
+            <h1>Login Component</h1>
+        )
+    }
+}
+
+export default Login;
