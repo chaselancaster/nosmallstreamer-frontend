984ee340a276e8310191e46b6e9597b79cddc003
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-01 18:01:08 -0800

    add Search folders, Search.js with initial test structyre, and Search.css files

---
+++ src/Component/Search/Search.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+    render() {
+        return (
+            <h1>Search Component</h1>
+        )
+    }
+}
+
+export default Search;
