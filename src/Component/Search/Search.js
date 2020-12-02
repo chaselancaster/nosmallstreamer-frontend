afe6e448ab88146c65a7ec03c719dbdd49023d27
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-01 18:22:43 -0800

    add Search structure with temporary CSS for functionality

--- src/Component/Search/Search.js
+++ src/Component/Search/Search.js
@@ -6,8 +6,19 @@ class Search extends Component {
     render() {
         return (
             <div>
-                 <h1>Search for streams!</h1>
-                 <p>Simply type in a game you want to see and the # of viewers you want to interact with!</p>
+                <div className="search-info">
+                    <h1>Search for streams!</h1>
+                    <p>Simply type in a game you want to see and the number of viewers you want to interact with!</p>
+                </div>
+                <div className="form-container">
+                    <form>
+                        <h3>Game:</h3>
+                        <input type="text" placeholder="Game" />
+                        <h3>Number of Viewers:</h3>
+                        <input type="number" placeholder="Viewers" />
+                        <button type="submit">Search</button>
+                    </form>
+                </div>
             </div>
         )
     }
