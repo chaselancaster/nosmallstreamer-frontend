e0672d647f90e7f8ae3d3b5184abb6dc8d5f0570
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-01 18:11:10 -0800

    add h1 and p tag to Search for context. Will move later

--- src/Component/Search/Search.js
+++ src/Component/Search/Search.js
@@ -5,7 +5,10 @@ import './Search.css';
 class Search extends Component {
     render() {
         return (
-            <h1>Search Component</h1>
+            <div>
+                 <h1>Search for streams!</h1>
+                 <p>Simply type in a game you want to see and the # of viewers you want to interact with!</p>
+            </div>
         )
     }
 }
