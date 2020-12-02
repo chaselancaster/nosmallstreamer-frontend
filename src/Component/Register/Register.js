3dd689839a6eefadd0d9a18ff0871c1ea6548678
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-01 16:42:55 -0800

    add test structure for Register.js

---
+++ src/Component/Register/Register.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+
+class Register extends Component {
+    render() {
+        return (
+            <h1>Register Component</h1>
+        )
+    }
+}
+
+export default Register;
