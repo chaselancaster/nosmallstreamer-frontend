8e50312457ed6e8d0e3aad72f73a3872b1439446
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-01 16:50:26 -0800

    create Login and Register css files

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 
+import './Login.css';
+
 class Login extends Component {
     render() {
         return (
--- src/Component/Register/Register.js
+++ src/Component/Register/Register.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 
+import './Register.css';
+
 class Register extends Component {
     render() {
         return (
