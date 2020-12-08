1da574d988a63510214d9dc57fc34a2696f180f6
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-08 15:53:35 -0800

    change login form to just be email instead of username. password is still there

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -5,7 +5,7 @@ import './Login.css';
 
 class Login extends Component {
     state = {
-        username: "",
+        email: "",
         password: "",
         logged: false,
         message: ""
@@ -18,14 +18,14 @@ class Login extends Component {
     }
 
     render() {
-        const { username, password } = this.state;
+        const { email, password } = this.state;
         return (
             <div className="login-parent-container">
                 <h1>Login:</h1>
                 <div className="login-form-container">
                     <form className="login-form">
-                        <h2>Username:</h2>
-                        <input type="text" name="username" value={username} onChange={this.changeHandler}/>
+                        <h2>Email:</h2>
+                        <input type="text" name="email" value={email} onChange={this.changeHandler}/>
                         <h2>Password:</h2>
                         <input type="password" name="password" value={password} onChange={this.changeHandler} />
                         <button type="submit" className="login-button">Submit</button>
