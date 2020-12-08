65f5469d28513934760e0d4ba69773f565d2e25e
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-08 15:21:46 -0800

    add ternary statement to Redirect to '/' if user successfully registers or to render the register form is logged is false

--- src/Component/Register/Register.js
+++ src/Component/Register/Register.js
@@ -46,22 +46,27 @@ class Register extends Component {
     render() {
         const { username, email, password } = this.state;
         return (
-            <div className="register-parent-container">
-                <h1>Register:</h1>
-                <div className="register-form-container">
-                    <form className="register-form" onSubmit={e => this.handleRegister(e)}>
-                        <h2>Username:</h2>
-                        <input type="text" name="username" value={username} onChange={this.changeHandler}/>
-                        <h2>Email:</h2>
-                        <input type="text" name="email" value={email} onChange={this.changeHandler} />
-                        <h2>Password:</h2>
-                        <input type="password" name="password" value={password} onChange={this.changeHandler} />
-                        <button type="submit" className="register-button">Submit</button>
-                        <h3>Already registered?</h3>
-                        <a href="/login"><p>Log in here!</p></a>
-                    </form>
+            <div>
+                {this.state.logged ? ( <Redirect to={"/"} /> ) : (
+                    <div className="register-parent-container">
+                    <h1>Register:</h1>
+                    <div className="register-form-container">
+                        <form className="register-form" onSubmit={e => this.handleRegister(e)}>
+                            <h2>Username:</h2>
+                            <input type="text" name="username" value={username} onChange={this.changeHandler}/>
+                            <h2>Email:</h2>
+                            <input type="text" name="email" value={email} onChange={this.changeHandler} />
+                            <h2>Password:</h2>
+                            <input type="password" name="password" value={password} onChange={this.changeHandler} />
+                            <button type="submit" className="register-button">Submit</button>
+                            <h3>Already registered?</h3>
+                            <a href="/login"><p>Log in here!</p></a>
+                        </form>
+                    </div>
                 </div>
+                )}
             </div>
+            
         )
     }
 }
