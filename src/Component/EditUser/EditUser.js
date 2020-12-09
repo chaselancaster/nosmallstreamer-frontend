03c3af2ba2a8005514f117fb589167cb112a5771
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-09 10:43:22 -0800

    add EditUser folder and EditUser.js file

---
+++ src/Component/EditUser/EditUser.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class EditUser extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Edit User</h1>
+            </div>
+        )
+    }
+}
+
+export default EditUser
