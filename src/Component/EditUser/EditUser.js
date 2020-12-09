e415a2dfa03855856d92f7e159e9464f86fa7097
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-09 11:11:24 -0800

    add some styling to thedit user form and add state to that component

--- src/Component/EditUser/EditUser.js
+++ src/Component/EditUser/EditUser.js
@@ -3,10 +3,28 @@ import React, { Component } from 'react'
 import './EditUser.css';
 
 class EditUser extends Component {
+    state = {
+        email: '',
+        password: '',
+        message: ''
+    }
+
     render() {
         return (
-            <div className="edit-user-parent">
-                <h1>Edit Your Email Or Passowrd</h1>
+            <div>
+                <div className="edit-user-header">
+                    <h1>Edit Your Email or Passowrd</h1>
+                </div>
+                <div className="edit-user-container">
+                    <form className="edit-form">
+                        <h3>Email:</h3>
+                        <input type="text" name="email"/>
+                        <h3>Password:</h3>
+                        <input type="password" name="password"/>
+                        <button type="submit" className="edit-button">Update Account</button>
+                        <p></p>
+                    </form>
+                </div>
             </div>
         )
     }
