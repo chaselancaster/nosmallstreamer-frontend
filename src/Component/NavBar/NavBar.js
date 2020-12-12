9e9b727914a5bbdc911e6f5d1aacf27010573169
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-11 16:46:41 -0800

    adjust mobile top of .nav-options

--- src/Component/NavBar/NavBar.js
+++ src/Component/NavBar/NavBar.js
@@ -13,46 +13,52 @@ const NavBar = ({ currentUser, doLogout }) => {
     const closeMobileMenu = () => setClick(false)
 
     return (
-        <div className='header'>
-            <div className='logo-nav'>
-                <div className='logo-container'>
-                    <a href='#'>
-                        <Logo className='logo' />
-                    </a>
-                </div>
-                <ul className={click ? 'nav-options active' : 'nav-options'}>
-                    <li className='option' onClick={closeMobileMenu}>
-                        <a href='#'>ABOUT</a>
-                    </li>
-                    <li className='option' onClick={closeMobileMenu}>
-                        <a href='#'>CONTACT</a>
-                    </li>
-                    <li className='option' onClick={closeMobileMenu}>
-                        <a href='#'>BLOG</a>
-                    </li>
-                    <li className='option mobile-option' onClick={closeMobileMenu}>
-                        <a href='#'>SIGNIN</a>
-                    </li>
-                    <li className='option mobile-option' onClick={closeMobileMenu}>
-                        <a href='#' className='sign-up'>SIGNUP</a>
-                    </li>
-                </ul>
-                <ul className='signin-up'>
-                    <li className='sign-in' onClick={closeMobileMenu}>
-                        <a href='#'>SIGN-IN</a>
-                    </li>
-                    <li onClick={closeMobileMenu}>
-                        <a href='#' className='signup-btn'>SIGN-UP</a>
-                    </li>
-                </ul>
-                <div className='mobile-menu' onClick={handleClick}>
-                    {click ? (
-                        <CloseMenu className='menu-icon' />
-                    ) : (
-                        <MenuIcon className='menu-icon' />
-                    )}
-                </div>
-            </div>
+        <div className="header">
+      <div className="logo-nav">
+        <div className="logo-container">
+          <a href="#">
+            <Logo className="logo" />
+          </a>
+        </div>
+
+        <ul className={click ? "nav-options active" : "nav-options"}>
+          <li className="option" onClick={closeMobileMenu}>
+            <a href="#">ABOUT</a>
+          </li>
+          <li className="option" onClick={closeMobileMenu}>
+            <a href="#">CONTACT</a>
+          </li>
+          <li className="option" onClick={closeMobileMenu}>
+            <a href="#">BLOG</a>
+          </li>
+          <li className="option mobile-option" onClick={closeMobileMenu}>
+            <a href="#">SIGN-IN</a>
+          </li>
+          <li className=" option mobile-option" onClick={closeMobileMenu}>
+            <a href="" className="sign-up">
+              SIGN-UP
+            </a>
+          </li>
+        </ul>
+      </div>
+      <ul className="signin-up">
+        <li className="sign-in" onClick={closeMobileMenu}>
+          <a href="#">SIGN-IN</a>
+        </li>
+        <li onClick={closeMobileMenu}>
+          <a href="" className="signup-btn">
+            SIGN-UP
+          </a>
+        </li>
+      </ul>
+      <div className="mobile-menu" onClick={handleClick}>
+        {click ? (
+          <CloseMenu className="menu-icon" />
+        ) : (
+          <MenuIcon className="menu-icon" />
+        )}
+      </div>
+   
 
 
 
