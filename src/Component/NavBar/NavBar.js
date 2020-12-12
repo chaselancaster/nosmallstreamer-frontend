c60687b5d2f1675b67e0d513f2c769a43cad2a8e
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-11 16:02:54 -0800

    add setClick, handleClick, and closeMobileMenu as well as additional className and onClick to stuff

--- src/Component/NavBar/NavBar.js
+++ src/Component/NavBar/NavBar.js
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ReactComponent as CloseMenu } from "./assets/x.svg";
+import { ReactComponent as MenuIcon } from "./assets/menu.svg";
+import { ReactComponent as Logo } from "./assets/logo.svg";
 import { BrowserRouter, NavLink, Link } from 'react-router-dom';
 import { slide as Menu } from 'react-burger-menu'
 
 import './NavBar.css';
 
 const NavBar = ({ currentUser, doLogout }) => {
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+    const closeMobileMenu = () => setClick(false)
+
     return (
-        <div>
-          
+        <div className='header'>
+            <div className='logo-nav'>
+                <div className='logo-container'>
+                    <a href='#'>
+                        <Logo className='logo' />
+                    </a>
+                </div>
+                <ul className={click ? 'nav-options active' : 'nav-options'}>
+                    <li className='option' onClick={closeMobileMenu}>
+                        <a href='#'>ABOUT</a>
+                    </li>
+                    <li className='option' onClick={closeMobileMenu}>
+                        <a href='#'>CONTACT</a>
+                    </li>
+                    <li className='option' onClick={closeMobileMenu}>
+                        <a href='#'>BLOG</a>
+                    </li>
+                    <li className='option mobile-option' onClick={closeMobileMenu}>
+                        <a href='#'>SIGNIN</a>
+                    </li>
+                    <li className='option mobile-option' onClick={closeMobileMenu}>
+                        <a href='#' className='sign-up'>SIGNUP</a>
+                    </li>
+                </ul>
+                <ul className='signin-up'>
+                    <li className='sign-in' onClick={closeMobileMenu}>
+                        <a href='#'>SIGN-IN</a>
+                    </li>
+                    <li onClick={closeMobileMenu}>
+                        <a href='#' className='signup-btn'>SIGN-UP</a>
+                    </li>
+                </ul>
+                <div className='mobile-menu' onClick={handleClick}>
+                    {click ? (
+                        <CloseMenu className='menu-icon' />
+                    ) : (
+                        <MenuIcon className='menu-icon' />
+                    )}
+                </div>
+            </div>
 
 
 
