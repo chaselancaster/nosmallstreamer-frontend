e54a38ffeaf1e1da0de4c49a40b7c808db63cbc3
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-15 13:50:27 -0800

    destructure width and height that is in the state in StreamsList.js

--- src/Component/StreamsList/StreamsList.js
+++ src/Component/StreamsList/StreamsList.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 class StreamsList extends Component {
     state = {
-        width: '450x',
-        height: '250.jpg'
+        width: '250x',
+        height: '100.jpg'
     }
 
+    imageHandler = string => {
+        let array = string.split("{");
+        array[1] = this.state.width;
+        array[2] = this.state.height;
+        let src = array.join("");
+        return src;
+      };
+
     render() {
+        const { width, height } = this.state
         return (
             <div>
                 Streams List component
