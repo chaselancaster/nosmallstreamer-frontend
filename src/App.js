63e6ae860c711fbe47a7d3574eb639ba68562962
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-18 14:48:15 -0800

    pass current user as props to the Search component

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends Component {
             <Route exact path={routes.LANDING} render={() => <Landing currentUser={this.state.currentUser}/>} />
             <Route exact path={routes.LOGIN} render={() => <Login doSetCurrentUser={this.doSetCurrentUser}/>} />
             <Route exact path={routes.REGISTER} render={() => <Register doSetCurrentUser={this.doSetCurrentUser}/>} />
-            <Route exact path={routes.SEARCH} render={() => <Search />} />
+            <Route exact path={routes.SEARCH} render={() => <Search currentUser={this.state.currentUser}/>} />
             <Route exact path={routes.PROFILE} render={() => <Profile currentUser={this.state.currentUser}/>} />
             <Route exact path={routes.EDITUSER} render={() => <EditUser currentUser={this.state.currentUser} doSetCurrentUser={this.doSetCurrentUser}/>}/>
           </Switch>
