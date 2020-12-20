753f5df8ef083987253dbfcdc12443d469b63e25
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-20 14:39:41 -0800

    commit to try and host

--- src/Component/StreamsList/StreamsList.js
+++ src/Component/StreamsList/StreamsList.js
@@ -1,67 +1,78 @@
-import React, { Component } from 'react'
-import InfiniteScroll from 'react-infinite-scroll-component';
+import React, { Component } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-import './StreamsList.css';
+import "./StreamsList.css";
 class StreamsList extends Component {
-    state = {
-        // 500 x 300
-        width: '325x',
-        height: '225.jpg',
-        windowWidth: 0
-    }
+  state = {
+    // 500 x 300
+    width: "325x",
+    height: "225.jpg",
+    windowWidth: 0,
+  };
 
-    imageHandler = string => {
-        let array = string.split("{");
-        array[1] = this.state.width;
-        array[2] = this.state.height;
-        let src = array.join("");
-        return src;
-      };
+  imageHandler = (string) => {
+    let array = string.split("{");
+    array[1] = this.state.width;
+    array[2] = this.state.height;
+    let src = array.join("");
+    return src;
+  };
 
-    updateDimensions = () => {
-        console.log('updateDimenions func')
-        this.setState({
-            windowWidth: window.innerWidth
-        })
-    }
+  updateDimensions = () => {
+    console.log("updateDimenions func");
+    this.setState({
+      windowWidth: window.innerWidth,
+    });
+  };
 
-    componentDidMount() {
-        console.log('componentDidMount')
-        window.addEventListener('resize', this.updateDimensions);
-    }
-    
-    componentWillUnmount() {
-        console.log('componentWillUnmount')
-        window.removeEventListener('resize', this.updateDimensions);
-    }
+  componentDidMount() {
+    console.log("componentDidMount");
+    window.addEventListener("resize", this.updateDimensions);
+  }
 
-    render() {
-        const { streams } = this.props
-        return (
-            // <div className='streams-container'>
-            <InfiniteScroll
-            className='streams-container'
-            dataLength={this.props.streams.length}
-            next={this.props.loadMoreStreams}
-            hasMore={true}
-            >
-                {streams.map(stream => {
-                    return (
-                        <li key={stream.id} className='stream'>
-                            <img src={this.imageHandler(stream.thumbnail_url)} />
-                            <div className='stream-info'>
-                                <p><span>Streamer: </span>{stream.user_name}</p>
-                                <p className='stream-title'>{stream.title}</p>
-                                <p><span>Viewers: </span>{stream.viewer_count}</p>
-                                <a href={`https://www.twitch.tv/${stream.user_name}`} target='_blank'><button className='view-button'>View on Twitch!</button></a>
-                            </div>
-                        </li>
-                    )
-                })}
-            </InfiniteScroll>
-            // </div>
-        )
-    }
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+    window.removeEventListener("resize", this.updateDimensions);
+  }
+
+  render() {
+    const { streams } = this.props;
+    return (
+      // <div className='streams-container'>
+      <InfiniteScroll
+        className="streams-container"
+        dataLength={this.props.streams.length}
+        next={this.props.loadMoreStreams}
+        hasMore={true}
+      >
+        {streams.map((stream) => {
+          return (
+            <li key={stream.id} className="stream">
+              <img src={this.imageHandler(stream.thumbnail_url)} />
+              <div className="stream-info">
+                <p>
+                  <span>Streamer: </span>
+                  {stream.user_name}
+                </p>
+                <p className="stream-title">{stream.title}</p>
+                <p>
+                  <span>Viewers: </span>
+                  {stream.viewer_count}
+                </p>
+                <a
+                  href={`https://www.twitch.tv/${stream.user_name}`}
+                  target="_blank"
+                >
+                  <button className="view-button">View on Twitch!</button>
+                </a>
+              </div>
+            </li>
+          );
+        })}
+      </InfiniteScroll>
+      // </div>
+    );
+  }
 }
 
-export default StreamsList
+export default StreamsList;
