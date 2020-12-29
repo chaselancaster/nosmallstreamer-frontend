67202b97b2f0a3075a49a9f7fd2ff03ef2c3ce2e
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-29 10:57:28 -0800

    add the beginning of getStreamers fetch call to see if backend is getting hit

--- src/Component/Leaderboard/Leaderboard.js
+++ src/Component/Leaderboard/Leaderboard.js
@@ -5,6 +5,23 @@ class Leaderboard extends Component {
         streamers: []
     }
 
+    getStreamers = async () => {
+        try {
+            const streamers = await fetch('http://localhost:3001/leaderboard/get', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+            })
+        } catch (err) {
+            console.log(err, '<- err in getStreamers in Leaderboard')
+        }
+    }
+
+    componentDidMount() {
+        this.getStreamers()
+    }
+
     render() {
         
         
