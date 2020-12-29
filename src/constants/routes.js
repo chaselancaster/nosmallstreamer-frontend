11cf7809bffca7ff3c97c7a85c6eb5630edf3bce
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-29 09:53:30 -0800

    add leaderboar constant to routes.js

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -4,3 +4,4 @@ export const PROFILE = "/profile";
 export const LOGIN = "/login";
 export const REGISTER = "/register";
 export const EDITUSER = "/edituser";
+export const LEADERBOARD = '/leaderboard'
