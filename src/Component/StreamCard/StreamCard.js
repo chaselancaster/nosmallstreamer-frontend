9210cc7357773950ae178fc5f2481df25b693640
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-30 16:20:05 -0800

    change rendering of li cards to rendering of StreamerCard components so that each can carry a state and update their own individual message

--- src/Component/StreamCard/StreamCard.js
+++ src/Component/StreamCard/StreamCard.js
@@ -1,10 +1,87 @@
 import React, { Component } from 'react'
+import { ReactComponent as ThumbsUp } from "../StreamsList/assets/thumbsUp.svg";
+import { ReactComponent as ThumbsDown } from "../StreamsList/assets/thumbsDown.svg";
+import { ReactComponent as PlusCircle } from '../StreamsList/assets/plusCircle.svg';
 
 class StreamCard extends Component {
+    state = {
+        // 500 x 300
+        width: "325x",
+        height: "225.jpg",
+        windowWidth: 0,
+        message: ''
+      };
+
+    upvote = 'upvote'
+    downvote = 'downvote'
+
+    imageHandler = (string) => {
+        let array = string.split("{");
+        array[1] = this.state.width;
+        array[2] = this.state.height;
+        let src = array.join("");
+        return src;
+    };
+
+    rateStreamer = async (name, vote) => {
+        try {
+        this.setState({
+            message: ''
+        })
+        const sendVote = await fetch(`http://localhost:3001/leaderboard/submit/${name}/${vote}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        })
+        const parsedSendVote = await sendVote.json()
+        if (parsedSendVote) {
+            this.setState({
+            message: parsedSendVote.message
+            })
+        }
+    } catch (err) {
+        console.log(err, '<- err in rateStreamer')
+    }
+    }
+
+    addToWatchLater = async (name) => {
+        // try {
+
+        // } catch (err) {
+        //   console.log(err, '<- err')
+        // }
+    }
+
     render() {
+        const { streamer } = this.props
         return (
             <div>
-                
+                    <li key={streamer.id} className="stream">
+                    <img src={this.imageHandler(streamer.thumbnail_url)} />
+                    <div className="stream-info">
+                        <p className="stream-user-name">
+                        {streamer.user_name}
+                        </p>
+                        <p className="stream-title">{streamer.title}</p>
+                        <p>
+                        <span>Viewers: </span>
+                        {streamer.viewer_count}
+                        </p>
+                        <a
+                        href={`https://www.twitch.tv/${streamer.user_name}`}
+                        target="_blank"
+                        >
+                        <button className="view-button">View on Twitch!</button>
+                        </a>
+                        <div className="stream-card-buttons">
+                        <ThumbsUp onClick={() => this.rateStreamer(streamer.user_name, this.upvote)} streamName={streamer.user_namee} className="thumbs-up"/>
+                        <PlusCircle className="plus-circle"/>
+                        <ThumbsDown onClick={() => this.rateStreamer(streamer.user_name, this.downvote)} streamName={streamer.user_namee} className="thumbs-down"/>
+                        </div>
+                        <p className="state-message">{this.state.message}</p>
+                    </div>
+                    </li>
             </div>
         )
     }
--- src/Component/StreamsList/StreamsList.js
+++ src/Component/StreamsList/StreamsList.js
@@ -7,72 +7,7 @@ import StreamCard from '../StreamCard/StreamCard';
 
 import "./StreamsList.css";
 class StreamsList extends Component {
-  state = {
-    // 500 x 300
-    width: "325x",
-    height: "225.jpg",
-    windowWidth: 0,
-    message: ''
-  };
-
-  upvote = 'upvote'
-  downvote = 'downvote'
-
-  imageHandler = (string) => {
-    let array = string.split("{");
-    array[1] = this.state.width;
-    array[2] = this.state.height;
-    let src = array.join("");
-    return src;
-  };
-
-  rateStreamer = async (name, vote) => {
-    try {
-      this.setState({
-        message: ''
-      })
-      const sendVote = await fetch(`http://localhost:3001/leaderboard/submit/${name}/${vote}`, {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-      }
-      })
-      const parsedSendVote = await sendVote.json()
-      if (parsedSendVote) {
-        this.setState({
-          message: parsedSendVote.message
-        })
-      }
-  } catch (err) {
-      console.log(err, '<- err in rateStreamer')
-  }
-  }
-
-  addToWatchLater = async (name) => {
-    // try {
-
-    // } catch (err) {
-    //   console.log(err, '<- err')
-    // }
-  }
-
-  updateDimensions = () => {
-    console.log("updateDimenions func");
-    this.setState({
-      windowWidth: window.innerWidth,
-    });
-  };
-
-  componentDidMount() {
-    console.log("componentDidMount");
-    window.addEventListener("resize", this.updateDimensions);
-  }
-
-  componentWillUnmount() {
-    console.log("componentWillUnmount");
-    window.removeEventListener("resize", this.updateDimensions);
-  }
-
+ 
   render() {
     const { streams } = this.props;
     return (
@@ -85,31 +20,7 @@ class StreamsList extends Component {
       >
         {streams.map((stream, index) => {
           return (
-            <li key={stream.id} className="stream">
-              <img src={this.imageHandler(stream.thumbnail_url)} />
-              <div className="stream-info">
-                <p className="stream-user-name">
-                  {stream.user_name}
-                </p>
-                <p className="stream-title">{stream.title}</p>
-                <p>
-                  <span>Viewers: </span>
-                  {stream.viewer_count}
-                </p>
-                <a
-                  href={`https://www.twitch.tv/${stream.user_name}`}
-                  target="_blank"
-                >
-                  <button className="view-button">View on Twitch!</button>
-                </a>
-                <div className="stream-card-buttons">
-                  <ThumbsUp onClick={() => this.rateStreamer(stream.user_name, this.upvote)} streamName={stream.user_namee} className="thumbs-up"/>
-                  <PlusCircle className="plus-circle"/>
-                  <ThumbsDown onClick={() => this.rateStreamer(stream.user_name, this.downvote)} streamName={stream.user_namee} className="thumbs-down"/>
-                </div>
-                <p className="state-message">{this.state.message}</p>
-              </div>
-            </li>
+            <StreamCard streamer={stream} />
           );
         })}
       </InfiniteScroll>
