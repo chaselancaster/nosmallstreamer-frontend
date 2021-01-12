15ef1462249876ea129fa1cf01e892c54abf2a09
Chase Lancaster <chaseblancaster@gmail.com>
2021-01-12 09:52:26 -0800

    refactor Profile to checkForUser and display the content or not

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ class App extends Component {
                 <Route exact path={routes.LANDING} render={() => <Landing />} />
                 <Route exact path={routes.LOGIN} render={() => <Login doSetCurrentUser={this.doSetCurrentUser} doSetUserToken={this.doSetUserToken}/>} />
                 <Route exact path={routes.REGISTER} render={() => <Register doSetCurrentUser={this.doSetCurrentUser} doSetUserToken={this.doSetUserToken}/>} />
-                <Route exact path={routes.SEARCH} render={() => <Search doSetCurrentUser={this.doSetCurrentUser} currentUser={this.state.currentUser} userToken={this.state.userToken}/>} />
+                <Route exact path={routes.SEARCH} render={() => <Search doSetCurrentUser={this.doSetCurrentUser} userToken={this.state.userToken}/>} />
                 <Route exact path={routes.PROFILE} render={() => <Profile currentUser={this.state.currentUser}/>} />
                 <Route exact path={routes.EDITUSER} render={() => <EditUser doSetCurrentUser={this.doSetCurrentUser}/>}/>
                 <Route exact path={routes.LEADERBOARD} render={() => <Leaderboard/>} />
--- src/Component/Profile/Profile.js
+++ src/Component/Profile/Profile.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import UserContext from '../../UserContext';
 
 import './Profile.css'
 class Profile extends Component {
-    render() {
-        const { currentUser } = this.props;
-        return (
-            <div className='profile-container'>
-                {!currentUser ? ( 
-                    <div className='profile-loggedout'>
-                        <h1>Please sign in or register to view profile!</h1>
-                        <Link to='login' className="profile-link"><button className='button'>Login</button></Link>
-                        <Link to='register' className="profile-link"><button className='button'>Register</button></Link>
-                    </div>
-                ) : (
-                    <div className="sections">
+
+    static contextType = UserContext
+
+    checkForUser = (user) => {
+        if (user) {
+            return (
+                <div className="sections">
                         <div className='profile-header'>
-                            <h1>Hello, <span>{currentUser.name}</span></h1>
+                            <h1>Hello, <span>{user.name}</span></h1>
                         </div>
                         <div className="profile-sections">
                             <div className="profile-search-parent section">
@@ -32,8 +28,24 @@ class Profile extends Component {
                                 <Link to='/watchlater'><button className="profile-button">Watch Later</button></Link>
                             </div>
                         </div>
-                    </div> 
-                )}
+                    </div>
+            )
+        } else {
+            return (
+                <div className='profile-loggedout'>
+                    <h1>Please sign in or register to view profile!</h1>
+                    <Link to='login' className="profile-link"><button className='button'>Login</button></Link>
+                    <Link to='register' className="profile-link"><button className='button'>Register</button></Link>
+                </div>
+            )
+        }
+    }
+
+    render() {
+        const user = this.context
+        return (
+            <div className='profile-container'>
+                {this.checkForUser(user)}
             </div>
         )
     }
